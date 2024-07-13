55d1329bcf8c4af174610ca59bfc732d86871ec4
Juan Carlos Gomez Jaramillo <juangomez88@gmail.com>
2024-07-12 20:26:58 -0500

    Header and start form

---
+++ src/views/ClientesView.vue
@@ -0,0 +1,25 @@
+<script setup>
+    import RouterLink from '../components/UI/RouterLink.vue';
+    import Heading from '../components/UI/Heading.vue'
+    
+    defineProps({
+        titulo: {
+            type: String
+        }
+    })
+
+</script>
+
+
+<template>
+    <div>
+        <div class="flex justify-end">
+            <RouterLink to="agregar-cliente">
+                Agregar Cliente
+            </RouterLink>
+        </div>
+
+        <Heading>{{ titulo }}</Heading>
+    </div>
+</template>
+
---
+++ src/views/NuevoClienteView.vue
@@ -0,0 +1,42 @@
+<script setup>
+    import { FormKit } from '@formkit/vue';
+    import RouterLink from '../components/UI/RouterLink.vue';
+    import Heading from '@/components/UI/Heading.vue';
+
+    defineProps({
+        titulo: {
+            type: String
+        }
+    })
+
+</script>
+
+
+<template>
+    <div>
+        <div class="flex justify-end">
+            <RouterLink to="inicio"
+            >
+                Volver
+            </RouterLink>
+        </div>
+        
+        <Heading>{{ titulo }}</Heading>
+
+        <div class="mx-auto mt-10 bg-white shadow">
+            <FormKit
+                type="form"
+            >
+                <FormKit 
+                    type="text"
+                    label="nombre"
+                    placeholder="Nombre del Cliente"
+                    help="Inregsar el nombre del cliente a registrar"
+                    validation="required"
+                    :validation-messages="{ required: 'El Nombre del Cliente es Obligatorio'}"
+                />
+            </FormKit>
+        </div>
+    </div>
+</template>
+
