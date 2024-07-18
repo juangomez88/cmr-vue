b72ece4de5cea4cac28e4753fcb2b79be1af1d5f
Juan Carlos Gomez Jaramillo <juangomez88@gmail.com>
2024-07-18 15:11:26 -0500

    Form

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import './assets/main.css'
 
 import { plugin, defaultConfig } from '@formkit/vue'
+
 import { createApp } from 'vue'
 import App from './App.vue'
+import config from '../formkit.config'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(router)
-app.use(plugin, defaultConfig())
+app.use(plugin, defaultConfig(config))
 
 app.mount('#app')
--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -1,13 +1,29 @@
 <script setup>
+    import axios from 'axios'
     import { FormKit } from '@formkit/vue';
+    import { useRouter } from 'vue-router';
     import RouterLink from '../components/UI/RouterLink.vue';
     import Heading from '@/components/UI/Heading.vue';
 
-    defineProps({
-        titulo: {
-            type: String
-        }
+
+const router = useRouter()
+
+defineProps({
+    titulo: {
+        type: String
+    }
+})
+
+
+const handleSubmit = (data) => {
+    axios.post('http://localhost:4000/clientes', data)
+    .then(respuesta => {
+        console.log(respuesta)
+        //Redicreccionar usuario
+        router.push({ name: 'inicio'})
     })
+    .catch(error => console.log(error))
+}
 
 </script>
 
@@ -15,28 +31,79 @@
 <template>
     <div>
         <div class="flex justify-end">
-            <RouterLink to="inicio"
-            >
+            <RouterLink to="inicio">
                 Volver
             </RouterLink>
         </div>
-        
+
         <Heading>{{ titulo }}</Heading>
 
-        <div class="mx-auto mt-10 bg-white shadow">
-            <FormKit
-                type="form"
-            >
+        <div class="mx-auto mt-10 bg-white shadow ">
+            <div class="mx-auto md:w-2/3 py-20 px-6">
                 <FormKit 
-                    type="text"
-                    label="nombre"
-                    placeholder="Nombre del Cliente"
-                    help="Inregsar el nombre del cliente a registrar"
-                    validation="required"
-                    :validation-messages="{ required: 'El Nombre del Cliente es Obligatorio'}"
-                />
-            </FormKit>
+                    type="form"
+                    submit-label="Agregar Cliente"
+                    incomlete-message="No se pudo enviar, revisa los mensajes"
+                    @submit="handleSubmit"
+                >
+                    <FormKit 
+                        type="text" 
+                        label="nombre"
+                        name="nombre"
+                        placeholder="Nombre del Cliente" 
+                        validation="required"
+                        :validation-messages="{ required: 'El Nombre del Cliente es Obligatorio' }" 
+                    />
+
+                    <FormKit 
+                        type="text" 
+                        label="Apellido"
+                        name="apellido"
+                        placeholder="Apellido del Cliente"
+                        validation="required"
+                        :validation-messages="{ required: 'El Apellido del Cliente es Obligatorio' }" 
+                    />
+
+                    <FormKit 
+                        type="email" 
+                        label="Email"
+                        name="email"
+                        placeholder="Email del Cliente" 
+                        validation="required|email"
+                        :validation-messages="{ required: 'El Email del Cliente es Obligatorio', email: 'Ingresa un email válido' }" 
+                    />
+
+                    <FormKit 
+                        type="text" 
+                        label="Teléfono"
+                        name="telefono"
+                        placeholder="Teléfono xxx-xxx-xxxx"
+                        validation="?matches:/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/"
+                        :validation-messages="{ matches: 'El formato no es válido'}" 
+                    />
+
+                    <FormKit 
+                        type="text" 
+                        label="Empresa"
+                        name="empresa"
+                        placeholder="Empresa del Cliente"
+                    />
+
+                    <FormKit 
+                        type="text" 
+                        label="Cargo"
+                        name="cargo"
+                        placeholder="Cargo del Cliente"
+                    />
+
+                </FormKit>
+            </div>
         </div>
     </div>
 </template>
 
+<style>
+.formkit-wrapper {
+    max-width: 100%;
+}
+</style>
